5b7f21b19fc37bb9a68e0901669de08d221524ea
qarrion <qarrion.k@gmail.com>
2023-11-19 20:17:57 +0900

    chore read_config warning fallback

--- util/readConfig.py
+++ util/readConfig.py
@@ -1,17 +1,18 @@
 import os
+import warnings
 from typing import Literal
 from configparser import ConfigParser, RawConfigParser
 
 def read_config(
         filename, 
         parser:Literal['config','rawconfig']='config',
-        location:Literal['file','project']='project'):
+        location:Literal['file','project']='project', 
+        ):
     """
     filename : <filename.ini>
     location : project -> project/config/
     """
     
-
     if location =='file':
         basedir = os.path.dirname(__file__)
     elif location =='project':
@@ -19,22 +20,33 @@ def read_config(
 
     filepath = os.path.join(basedir, filename)
 
+    # -------------------------------- isfile -------------------------------- #
     if not os.path.isfile(filepath):
-        msg = f"No file ({filepath}))"
-        cred = '\033[91m'
-        cend = '\033[0m'
-        print(cred+msg+cend)
-        return()
-    
+        msg = f":::No {filename} file in 'project/config/':::"
+        warnings.warn(msg)
+        return None
+    # ------------------------------------------------------------------------ #
+
+
     if parser == 'config':
         conf = ConfigParser()
     elif parser == 'rawconfig':
         conf = RawConfigParser()
 
     conf.read(filepath)
+
+    # ------------------------------- isdefault ------------------------------ #
+    if 'default' not in conf.sections():
+        warnings.warn(":::No 'default' section:::")
+    # ------------------------------------------------------------------------ #
+
     return conf
 
 if __name__ == "__main__":
-    config = read_config('myconfig.ini')
-    print(config['test']['var1'])
-    print(config.get('test','var1'))
+    config = read_config('test.ini')
+    # config = read_config('myconfig.ini')
+    # print(config['test']['var1'])
+    # print(config.get('test','var1'))
+   
+    for s in  config.sections():
+        print(s)
